514629851314df39611b8a597dc753b3e4e2423a
SaadTanveer14 <99664282+SaadTanveer14@users.noreply.github.com>
2023-06-23 16:10:51 +0500

    Sata Code Added

---
+++ lib/sata.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Sata extends StatefulWidget {
+  const Sata({super.key});
+
+  @override
+  State<Sata> createState() => _SataState();
+}
+
+class _SataState extends State<Sata> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
