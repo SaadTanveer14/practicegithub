e5377ab809320d9322772a4994d43d7334390857
SaadTanveer14 <99664282+SaadTanveer14@users.noreply.github.com>
2023-06-23 16:25:47 +0500

    sata changed

--- lib/sata.dart
+++ lib/sata.dart
@@ -10,6 +10,6 @@ class Sata extends StatefulWidget {
 class _SataState extends State<Sata> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
